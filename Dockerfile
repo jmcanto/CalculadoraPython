dc1a9ab493b9796b7268ed367ad5edd5f434ccf0
jmcanto <jmcanto@gmail.com>
2023-11-06 22:53:48 +0100

    cambios en el dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM alpine:latest
-
+env operacion = 3
+env operador1 = 40
+env operador2 = 656
 # después de actualizar y instalar python se borra la caché de los paquetes
 RUN apk update && apk add python3 && rm -rf /var/cache/apt/*
 
 WORKDIR /calculadora
 COPY .  /calculadora
-<<<<<<< HEAD
-ENTRYPOINT ["python", "./calculadora.py"]
-=======
->>>>>>> 57639672d06bdbeefa33f7e44c6c0e3bcb14c3c7
+CMD ["python", "./calculadora.py", "-o", $operacion, "-n1", $operador1, "-n2", $operador2]
--- calculadora.py
+++ calculadora.py
@@ -15,24 +15,25 @@ def multiplicacion(num1, num2):
 # división de 2 números
 def division(num1, num2):
     return num1 / num2
-
-
  
-parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--operacion", help="Tipo de Operacion")
-parser.add_argument("-n1", "--numero1", help="Primer operador")
-parser.add_argument("-n2", "--numero2", help="Segundo operador")
+parser = argparse.ArgumentParser(description='Calculadora recibiendo parámetros')
+parser.add_argument('-o', '--operacion', help='Tipo de Operacion')
+parser.add_argument('-n1', '--numero1', type=int, help='Primer operador')
+parser.add_argument('-n2', '--numero2', type=int, help='Segundo operador')
 args = parser.parse_args()
 
-print("Operacion:")
+print("Operaciones disponibles:")
 print("1. Suma")
 print("2. Resta")
 print("3. Multiplicación")
 print("4. División")
-opcion = input("Indique una opción (1/2/3/4): ")
+opcion = args.operacion # input("Indique una opción (1/2/3/4): ")
+num1 = args.numero1
+num2 = args.numero2
+#num1 = float(input("Primer número: "))
+#num2 = float(input("Segundo número: "))
 
-num1 = float(input("Primer número: "))
-num2 = float(input("Segundo número: "))
+print(opcion)
 
 if opcion == '1':
     print(num1, "+", num2, "=", suma(num1, num2))
